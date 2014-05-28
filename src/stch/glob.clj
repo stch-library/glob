c24855fb9875156dfea27e52180eeff1c8d38f5e
David Sargeant <dsargeant@roomkey.com>
2014-05-28 18:34:54 -0400

    Added globf for file matching.

--- src/stch/glob.clj
+++ src/stch/glob.clj
@@ -40,3 +40,41 @@
     (if (string? matches)
       matches
       (first matches))))
+
+(defn- split-on-slash [s]
+  (->> (string/split s #"/")
+       (remove string/blank?)))
+
+(defn- absolute? [s]
+  (= (first s) \/))
+
+(defn negate [form]
+  `(not ~form))
+
+(defmacro and-not [& forms]
+  `(and ~@(map negate forms)))
+
+(defn globf
+  "Attempt to match the file path with the given glob pattern.
+  Returns the matched portion on success, otherwise nil.
+  Pattern and path must be strings."
+  [pattern path]
+  (let [abs-path? (absolute? path)
+        abs-pattern? (absolute? pattern)]
+    (when (or (and abs-path? abs-pattern?)
+              (and-not abs-path? abs-pattern?))
+      (let [pattern-parts (split-on-slash pattern)
+            path-parts (split-on-slash path)
+            comb (->> (interleave pattern-parts path-parts)
+                      (partition 2))
+            path-matches
+            (for [[pattern path] comb
+                  :let [regex (-> (glob-pattern pattern)
+                                  :compiled-pattern)
+                        matches (re-matches regex path)]
+                  :while matches]
+              path)]
+        (when (= (count path-matches)
+                 (count pattern-parts))
+          (str (when abs-path? "/")
+               (string/join "/" path-matches)))))))
